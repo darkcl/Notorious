df049c9512026b78734d00c27741491dab8621c6
Yeung Yiu Hung <hkclex@gmail.com>
2019-07-31 00:36:29 +0800

    clean up

--- ui/src/interface/IEditorPageState.ts
+++
@@ -1,4 +0,0 @@
-export interface IEditorPageState {
-  markdownSrc: string;
-  isPreview: boolean;
-}
--- ui/src/page/EditorPage.tsx
+++ ui/src/page/EditorPage.tsx
@@ -1,19 +1,15 @@
 import * as React from "react";
-import { useContext } from "react";
 import * as ReactMarkdown from "react-markdown/with-html";
 
 import "../style/EditorPage.css";
-import { IEditorPageState } from "../interface/IEditorPageState";
 
 import { Editor } from "../components/Editor";
 import { CodeBlock } from "../components/CodeBlock";
-import { AppContext } from "../state/AppContext";
 
 export const EditorPage = props => {
   const [markdown, setMarkdown] = React.useState(
     "# Hello World\n\n[google](http://google.com)"
   );
-  const { isPreview, dispatch } = React.useContext(AppContext);
   return (
     <div className="editor-page-content">
       {props.isPreview ? (
@@ -27,7 +23,6 @@ export const EditorPage = props => {
           className="editor"
           value={markdown}
           onChange={value => setMarkdown(value)}
-          onKeyDown={this.onKeyPressed}
         />
       )}
     </div>
