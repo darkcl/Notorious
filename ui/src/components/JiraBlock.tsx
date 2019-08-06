a3581bf9b2c13829f5687a3751d29460eefa2cb5
Yeung Yiu Hung <hkclex@gmail.com>
2019-08-06 18:27:54 +0800

    feat(jira_block): add load more button

--- ui/src/components/JiraBlock.tsx
+++ ui/src/components/JiraBlock.tsx
@@ -1,33 +1,48 @@
 import * as React from "react";
 import Button from "@atlaskit/button";
+import { ButtonGroup } from "@atlaskit/button";
+import Tooltip from "@atlaskit/tooltip";
 
 declare var settings;
 declare var external;
+
+const Row = (props: React.HTMLProps<HTMLDivElement>) => (
+  <div css={{ padding: 8 }} {...props} />
+);
 
 export class JiraBlock extends React.Component<{ issueKey: string }, {}> {
   constructor(props) {
     super(props);
   }
 
+  onIssueButtonClick = () => {
+    if (
+      settings.data.settings.jira.baseUrl !== null &&
+      settings.data.settings.jira.baseUrl !== undefined
+    ) {
+      external.invoke(
+        "openlink: " +
+          `${settings.data.settings.jira.baseUrl}/browse/${this.props.issueKey}`
+      );
+    }
+  };
+
   render() {
     return (
-      <Button
-        onClick={() => {
-          if (
-            settings.data.settings.jira.baseUrl !== null &&
-            settings.data.settings.jira.baseUrl !== undefined
-          ) {
-            external.invoke(
-              "openlink: " +
-                `${settings.data.settings.jira.baseUrl}/browse/${
-                  this.props.issueKey
-                }`
-            );
-          }
-        }}
-      >
-        Open <strong>{this.props.issueKey}</strong>
-      </Button>
+      <div>
+        <Row>
+          <Row>
+            <ButtonGroup appearance="default">
+              <Button onClick={this.onIssueButtonClick}>
+                Open <strong>{this.props.issueKey}</strong>
+              </Button>
+              <Tooltip content="Load more...">
+                <Button>...</Button>
+              </Tooltip>
+            </ButtonGroup>
+          </Row>
+        </Row>
+      </div>
     );
   }
 }
